9d1662000b3afebae62249d99fa62eaa483a0699
schimmelc <schimmelc4422@my.uwstout.edu>
2014-10-01 20:33:06 -0500

    Other stuff for clear button

---
+++ Booth's Algorithm/Assets/ClearInputAndOutput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClearInputAndOutput : MonoBehaviour {
+
+
+    public GameObject input1;
+    public GameObject input2;
+    public GameObject output;
+
+	// Use this for initialization
+	void Start () 
+    {
+        output.GetComponent<Text>().text = "Output";
+        input1.GetComponentInChildren<Text>().text = "";
+        input1.GetComponent<InputField>().value = "";
+        input2.GetComponentInChildren<Text>().text = "";
+        input2.GetComponent<InputField>().value = "";
+        this.enabled = false;
+	}
+
+    void OnEnable()
+    {
+        this.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
